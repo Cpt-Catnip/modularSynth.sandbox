d20360110922a87d972fa2d9a8b069270119ec61
Mike Koshakow <mzkosh@gmail.com>
2020-07-29 17:03:25 -0400

    I think I get it now

--- client/components/PlayButton.js
+++ client/components/PlayButton.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { Destination, start } from 'tone';
+// import React from 'react';
+// import { Destination, start } from 'tone';
 
-class PlayButton extends React.Component {
-  constructor() {
-    super();
-    this.state = { isPlaying: false };
-    this.toggleAudio = this.toggleAudio.bind(this);
-  }
+// class PlayButton extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = { isPlaying: false };
+//     this.toggleAudio = this.toggleAudio.bind(this);
+//   }
 
-  toggleAudio() {
-    if (this.state.isPlaying) {
-      console.log("stopping")
-      Destination.mute = true;
-      this.setState({ isPlaying: false });
-    } else {
-      console.log('starting');
-      start();
-      Destination.mute = false;
-      this.setState({ isPlaying: true });
-    }
-  }
+//   toggleAudio() {
+//     if (this.state.isPlaying) {
+//       console.log("stopping")
+//       Destination.mute = true;
+//       this.setState({ isPlaying: false });
+//     } else {
+//       console.log('starting');
+//       start();
+//       Destination.mute = false;
+//       this.setState({ isPlaying: true });
+//     }
+//   }
 
-  render() {
-    return (
-      <button type="button" onClick={this.toggleAudio}>
-        button
-      </button>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <button type="button" onClick={this.toggleAudio}>
+//         button
+//       </button>
+//     );
+//   }
+// }
 
-export default PlayButton;
+// export default PlayButton;
--- client/components/SimpleOscillator.js
+++ client/components/SimpleOscillator.js
@@ -2,9 +2,27 @@ import React from 'react';
 import { Oscillator } from 'tone';
 
 const SimpleOscillator = () => {
-  const osc = new Oscillator(440, 'sine').toDestination().start();
+  const osc = new Oscillator().toDestination();
+  let isPlaying = false;
 
-  return <h1>A Simple Oscillator</h1>;
+  const start = () => {
+    if (isPlaying) {
+      osc.stop();
+      isPlaying = false;
+    } else {
+      osc.start();
+      isPlaying = true;
+    }
+  };
+
+  return (
+    <div>
+      <h1>A Simple Oscillator</h1>
+      <button type="button" onClick={start}>
+        Play
+      </button>
+    </div>
+  );
 };
 
 export default SimpleOscillator;
